5376d611c4c1c316e4b468be1a56f5f4478ab7a1
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-08-02 00:41:50 +0530

    Create `pybamm-cookiecutter` using `cookiecutter`

---
+++ src/pybamm_cookiecutter/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright (c) 2023 Agriya Khetarpal. All rights reserved.
+
+pybamm-cookiecutter: A template for creating battery modeling projects based on PyBaMM
+"""
+
+
+__version__ = "0.1.0"
+
+__all__ = ("__version__",)
