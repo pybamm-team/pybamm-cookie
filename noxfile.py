647504352ca058b24a82e864ee3ddddfd8ba2b1f
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2024-06-09 22:43:52 +0530

    added tests for template generation in ci

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 import nox
 
 # Options to modify nox behaviour
+nox.options.default_venv_backend = "uv|virtualenv"
 nox.options.reuse_existing_virtualenvs = True
 
 @nox.session(name="docs")
@@ -18,3 +19,12 @@ def build_docs(session: nox.Session) -> None:
             ".",
             f"{envbindir}/../tmp/html",
         )
+
+@nox.session(name="test-generation")
+def run_template_generation(session):
+    """Run the tests tests for testing template generation"""
+    session.install("setuptools", silent=False)
+    session.install("pytest", silent=False)
+    session.install("pytest-cookies", silent=False)
+    session.install("-e", ".", silent=False)
+    session.run("pytest", "tests")
