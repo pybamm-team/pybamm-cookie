2f51084ec3229e16ed558c36e820deef8bf24220
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2024-06-12 20:58:07 +0530

    modified test cases for bake()

--- tests/test_project_generation.py
+++ tests/test_project_generation.py
@@ -5,25 +5,32 @@ from pytest_cookies.plugin import Cookies
 def test_version() -> None:
     assert m.__version__
 
-@pytest.fixture
-def custom_template(tmpdir):
+def test_bake_project(cookies: Cookies):
     """
-    Generating a project using the template into a tempdir
+    Testing the template generation with default values
     """
-    template = tmpdir.ensure("cookiecutter-template", dir=True)
-    template.join("cookiecutter.json").write('{"project_name": "pybamm_cookie"}')
-
-    repo_dir = template.ensure("{{cookiecutter.project_name}}", dir=True)
-    repo_dir.join("README.rst").write("{{cookiecutter.project_name}}")
-
-    return template
+    result = cookies.bake()
+    assert result.exit_code == 0, f"Exited with code {result.exit_code}, expected 0"
+    assert result.exception is None, result.exception
+    assert result.project_path.name == "pybamm-example-project"
+    assert result.project_path.is_dir(), f"Project directory {result.project_path} not found"
 
 
-def test_bake_custom_project(cookies: Cookies, custom_template):
+def test_bake_custom_project(cookies: Cookies):
     """
-    Testing if the projects exists in the tempdir
+    Testing the template generation with custom template and checking if the projects exists in the tmp_path
     """
-    result = cookies.bake(template=str(custom_template))
+    result = cookies.bake(extra_context={
+        "author_full_name": "pybamm_user",
+        "author_email": "pybamm@pybamm.org",
+        "project_name": "pybamm_cookie",
+        "project_slug": "example",
+        "project_short_description": "This is an example pybamm cookiecutter template",
+        "project_url": "pybamm.org",
+        "project_version": "0.1.0",
+        "documentation_engine": "sphinx(rst)",
+    })
+
     assert result.exit_code == 0, f"Exited with code {result.exit_code}, expected 0"
     assert result.exception is None, result.exception
     assert result.project_path.name == "pybamm_cookie"
