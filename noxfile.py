5374988c46750731d5c853d86bb5a42050eab220
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2024-06-21 01:35:23 +0530

    added parameter entry points

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,12 @@
 import nox
+from pathlib import Path
+import os
 
 # Options to modify nox behaviour
 nox.options.default_venv_backend = "uv|virtualenv"
 nox.options.reuse_existing_virtualenvs = True
+
+VENV_DIR = Path("./venv").resolve()
 
 @nox.session(name="docs")
 def build_docs(session: nox.Session) -> None:
@@ -26,3 +30,11 @@ def run_template_generation(session):
     session.install("setuptools", silent=False)
     session.install("-e", ".[dev]", silent=False)
     session.run("pytest", "tests")
+
+@nox.session(name="dev")
+def set_dev(session):
+    """Install pybamm-cookiecutter in editable mode"""
+    session.install("virtualenv")
+    session.run("virtualenv", os.fsdecode(VENV_DIR), silent=True)
+    python = os.fsdecode(VENV_DIR.joinpath("bin/python"))
+    session.run(python, "-m", "pip", "install", "-e", ".[dev]")
---
+++ tests/test_entry_points.py
@@ -0,0 +1,27 @@
+import pytest
+import pybamm_cookiecutter
+import glob
+import importlib.util
+import sys
+
+def test_entry_points():
+    """Testing if the entry points are loading right"""
+
+    entry_points = list(pybamm_cookiecutter.parameter_sets)
+    parameter_sets = glob.glob("src/pybamm_cookiecutter/parameters/input/*.py")
+    # Making a list Parameter sets in the parameters/input directory
+    parameter_sets = [x.split('/')[-1].split('.py')[0] for x in parameter_sets]
+
+    assert parameter_sets == entry_points, "Entry points missing either in pyproject.toml or in the input directory"
+
+def test_entry_point_load():
+    """Testing if the values get loaded via parameter entry points and are equal when loaded through entry points"""
+    # Loading parameter_sets through entry points
+    parameters = pybamm_cookiecutter.parameter_sets['Chen2020']
+    # Loading parameter sets through the source file by dynamically loading Chen2020.py as a module
+    spec = importlib.util.spec_from_file_location("Chen2020mod", "src/pybamm_cookiecutter/parameters/input/Chen2020.py")
+    chen_module = importlib.util.module_from_spec(spec)
+    sys.modules["Chen2020mod"] = chen_module
+    spec.loader.exec_module(chen_module)
+    parameters_from_file = chen_module.get_parameter_values()
+    assert parameters.keys() == parameters_from_file.keys(), "The keys in the module and local input file are not the same"
