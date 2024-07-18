14ac3a2a783bcf6fd791982fe125ee50ed86d0df
Santhosh <52504160+santacodes@users.noreply.github.com>
2024-07-18 18:23:14 +0530

    Added Model Entry Points (#23)
    + Implemented `Model Entry Points` to create Model objects within the
    template and initialise them through entry points.
    + Added the `SPM` model which can be initialised through the model entry
    points.
    + A wrapper method to load a model object called
    `models("modelname/authorname")` is added.
    Example - 
    To load the `SPM` model, after installing the `pybamm_cookiecutter`
    project, it can be accessed by calling,
    `pybamm_cookiecutter.Model("SPM")`. This would return an initialised
    model object of the `SPM` model.
    + Added two basic tests for model entry points.
    
    ---------
    
    Co-authored-by: Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
    Co-authored-by: Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>

--- src/pybamm_cookiecutter/entry_point.py
+++ src/pybamm_cookiecutter/entry_point.py
@@ -37,10 +37,11 @@ import textwrap
 from collections.abc import Mapping
 from typing import Callable
 
-class ParameterSets(Mapping):
+class EntryPoint(Mapping):
     """
-    Dict-like interface for accessing parameter sets through entry points in cookiecutter template.
-    Access via :py:data:`pybamm_cookiecutter.parameter_sets`
+    Dict-like interface for accessing parameter sets and models through entry points in cookiecutter template.
+    Access via :py:data:`pybamm_cookiecutter.parameter_sets` for parameter_sets
+    Access via :py:data:`pybamm_cookiecutter.Model` for Models
 
     Examples
     --------
@@ -48,8 +49,10 @@ class ParameterSets(Mapping):
         >>> import pybamm_cookiecutter
         >>> list(pybamm_cookiecutter.parameter_sets)
         ['Chen2020', ...]
+        >>> list(pybamm_cookiecutter.models)
+        ['SPM', ...]
 
-    Get the docstring for a parameter set:
+    Get the docstring for a parameter set/model:
 
 
         >>> print(pybamm_cookiecutter.parameter_sets.get_docstring("Ai2020"))
@@ -58,15 +61,23 @@ class ParameterSets(Mapping):
         :footcite:t:`rieger2016new` and references therein.
         ...
 
-    See also: :ref:`adding-parameter-sets`
-
+        >>> print(pybamm_cookiecutter.models.get_docstring("SPM"))
+        <BLANKLINE>
+        Single Particle Model (SPM) model of a lithium-ion battery, from :footcite:t:`Marquis2019`. This class differs from the :class:`pybamm.lithium_ion.SPM` model class in that it shows the whole model in a single class. This comes at the cost of flexibility in combining different physical effects, and in general the main SPM class should be used instead.
+        ...
+        See also: :ref:`adding-parameter-sets`
     """
 
-    def __init__(self):
-        """Dict of entry points for parameter sets, lazily load entry points as"""
-        self.__all_parameter_sets = dict()
-        for entry_point in self.get_entries("cookie_parameter_sets"):
-            self.__all_parameter_sets[entry_point.name] = entry_point
+    _instances = 0
+    def __init__(self, group):
+        """Dict of entry points for parameter sets or models, lazily load entry points as"""
+        if not hasattr(self, 'initialized'):    # Ensure __init__ is called once per instance
+            self.initialized = True
+            EntryPoint._instances += 1
+            self._all_entries = dict()
+            self.group = group
+            for entry_point in self.get_entries(self.group):
+                self._all_entries[entry_point.name] = entry_point
 
     @staticmethod
     def get_entries(group_name):
@@ -76,9 +87,9 @@ class ParameterSets(Mapping):
         else:
             return importlib.metadata.entry_points(group=group_name)
 
-    def __new__(cls):
-        """Ensure only one instance of ParameterSets exists"""
-        if not hasattr(cls, "instance"):
+    def __new__(cls, group):
+        """Ensure only two instances of entry points exist, one for parameter sets and the other for models"""
+        if EntryPoint._instances < 2:
             cls.instance = super().__new__(cls)
         return cls.instance
 
@@ -86,25 +97,25 @@ class ParameterSets(Mapping):
         return self._load_entry_point(key)()
 
     def _load_entry_point(self, key) -> Callable:
-        """Check that ``key`` is a registered ``cookie_parameter_sets``,
-        and return the entry point for the parameter set, loading it needed."""
-        if key not in self.__all_parameter_sets:
-            raise KeyError(f"Unknown parameter set: {key}")
-        ps = self.__all_parameter_sets[key]
+        """Check that ``key`` is a registered ``parameter_sets`` or ``models` ,
+        and return the entry point for the parameter set/model, loading it needed."""
+        if key not in self._all_entries:
+            raise KeyError(f"Unknown parameter set or model: {key}")
+        ps = self._all_entries[key]
         try:
-            ps = self.__all_parameter_sets[key] = ps.load()
+            ps = self._all_entries[key] = ps.load()
         except AttributeError:
             pass
         return ps
 
     def __iter__(self):
-        return self.__all_parameter_sets.__iter__()
+        return self._all_entries.__iter__()
 
     def __len__(self) -> int:
-        return len(self.__all_parameter_sets)
+        return len(self._all_entries)
 
     def get_docstring(self, key):
-        """Return the docstring for the ``key`` parameter set"""
+        """Return the docstring for the ``key`` parameter set or model"""
         return textwrap.dedent(self._load_entry_point(key).__doc__)
 
     def __getattribute__(self, name):
@@ -114,4 +125,30 @@ class ParameterSets(Mapping):
               raise error
 
 #: Singleton Instance of :class:ParameterSets """
-parameter_sets = ParameterSets()
+parameter_sets = EntryPoint(group="parameter_sets")
+
+#: Singleton Instance of :class:ModelEntryPoints"""
+models = EntryPoint(group="models")
+
+def Model(model:str):
+    """
+    Returns the loaded model object
+
+    Parameters
+    ----------
+    model : str
+        The model name or author name of the model mentioned at the model entry point.
+    Returns
+    -------
+    pybamm.model
+        Model object of the initialised model.
+    Examples
+    --------
+    Listing available models:
+        >>> import pybamm_cookiecutter
+        >>> list(pybamm_cookiecutter.models)
+        ['SPM', ...]
+        >>> pybamm_cookiecutter.Model('Author/Year')
+        <pybamm_cookiecutter.models.input.SPM.SPM object>
+    """
+    return models[model]
--- tests/test_entry_points.py
+++ tests/test_entry_points.py
@@ -4,8 +4,8 @@ import importlib.util
 import sys
 from pathlib import Path
 
-def test_entry_points():
-    """Test if the entry points are loaded correctly."""
+def test_parameter_sets_entry_points():
+    """Test if the parameter_sets via entry points are loaded correctly."""
 
     entry_points = list(pybamm_cookiecutter.parameter_sets)
     parameter_sets = Path("src/pybamm_cookiecutter/parameters/input/").glob("*.py")
@@ -14,7 +14,7 @@ def test_entry_points():
 
     assert parameter_sets == entry_points, "Entry points missing either in pyproject.toml or in the input directory"
 
-def test_entry_point_load():
+def test_parameter_sets_entry_point_load():
     """Testing if the values get loaded via parameter entry points and are equal when loaded through entry points"""
     # Loading parameter_sets through entry points
     parameters = pybamm_cookiecutter.parameter_sets['Chen2020']
@@ -25,3 +25,19 @@ def test_entry_point_load():
     spec.loader.exec_module(chen_module)
     parameters_from_file = chen_module.get_parameter_values()
     assert parameters.keys() == parameters_from_file.keys(), f"The keys in the module and local input file are not the same, expected {parameters.keys} got {parameters_from_file.keys()}"
+
+def test_model_entry_points():
+    """Test if the models via entry points are loaded correctly."""
+
+    entry_points = list(pybamm_cookiecutter.models)
+    models = Path("src/pybamm_cookiecutter/models/input/").glob("*.py")
+    # Making a list Parameter sets in the parameters/input directory
+    models = [x.stem for x in models]
+
+    assert models == entry_points, "Entry points missing either in pyproject.toml or in the input directory"
+
+def test_model_entry_point_load():
+    """Testing if the model gets initialised and returned."""
+    # Loading parameter_sets through entry points
+    model_instance = pybamm_cookiecutter.Model("SPM")
+    assert model_instance is not None
