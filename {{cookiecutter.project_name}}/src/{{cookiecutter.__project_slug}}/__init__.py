ef3ef2f7192f329ae25af12e6b9ddb265abfdb49
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2024-07-26 23:58:39 +0530

    changed vcs configuration

--- {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/__init__.py
@@ -7,7 +7,7 @@ Copyright (c) {{ cookiecutter.__year }} {{ cookiecutter.full_name }}. All rights
 from __future__ import annotations
 {%- endif %}
 
-{%- if cookiecutter.hatch_vcs %}
+{%- if cookiecutter.vcs %}
 from ._version import version as __version__
 {%- else %}
 __version__ = "0.1.0"
@@ -20,7 +20,7 @@ __all__: list[str] = [
 {%- else %}
 __all__ = [
 {%- endif %}
-{%- if cookiecutter.hatch_vcs %}
+{%- if cookiecutter.vcs %}
     "__version__",
 {%- endif %}
     "pybamm",
